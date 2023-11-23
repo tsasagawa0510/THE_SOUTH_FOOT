2daf5644ed3565c96ffbb781b8cb18ac1efe70cb
tsasagawa0510 <mail@tsubasa-sasagawa.com>
2023-11-23 10:15:49 +0900

    add

--- js/test.js
+++ js/test.js
@@ -57,3 +57,59 @@ $('.carousel').slick({
   autoplaySpeed: 5000,
   arrows: false,
 });
+
+$('.category-slick').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+				
+// window.addEventListener('resize', () => {
+//   console.log(window.innerWidth);
+//   if ((window.innerWidth <= 768) && humburgerMenuWrap.classList.contains('active')) {
+//     humburgerMenuWrap.classList.remove('active');
+//   }
+// });
+
+// 768px以下になったらカテゴリーのslickを解除する。
+// slickクラスがあるか判定、真ならslickクラス解除して2×4の表示(flex)
+
+
+
+
+
+
+
+
+
+
