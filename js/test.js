8dc559c4385b56192cd1348929a0381b82d815f0
tsasagawa0510 <mail@tsubasa-sasagawa.com>
2023-11-25 00:49:58 +0900

    add

--- js/test.js
+++ js/test.js
@@ -59,40 +59,43 @@ $('.carousel').slick({
 });
 
 $('.category-slick').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
+  // dots: true,
+  // infinite: false,
+  // slidesToShow: 4,
+  // slidesToScroll: 4,
+  // responsive: [
+  //   {
+  //     breakpoint: 1024,
+  //     settings: {
+  //       slidesToShow: 3,
+  //       slidesToScroll: 3,
+  //       infinite: true,
+  //       dots: true
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 780,
+  //     settings: {
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1,
+  //       infinite: false,
+  //       dots: false,
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: "unslick",
+  //   }
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
-  ]
+  // ]
 });
+
+// $(window).on("resize orientationchange", function() {
+//   $('.category-slick').slick("resize");
+//   console.log('DAAAAAAAAA');
+// });
 				
 // window.addEventListener('resize', () => {
 //   console.log(window.innerWidth);
